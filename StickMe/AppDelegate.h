ac7e13a9e9ec36f062711a6ccc50da15e9f0f6de
samthui7 <samthui7@gmail.com>
2012-12-11 11:36:16 +0700

    Initial Commit

---
+++ StickMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StickMe
+//
+//  Created by admin on 12/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
