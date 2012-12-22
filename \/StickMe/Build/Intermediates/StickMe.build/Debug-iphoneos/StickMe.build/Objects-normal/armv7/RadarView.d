4afdeaa2a70b949dadbaa12cc11f1eccdbdb5fab
samthui7 <samthui7@gmail.com>
2012-12-22 11:05:35 +0700

    Commit missed file
    "WaveStrengthView.xib"

--- \/StickMe/Build/Intermediates/StickMe.build/Debug-iphoneos/StickMe.build/Objects-normal/armv7/RadarView.d
+++ \/StickMe/Build/Intermediates/StickMe.build/Debug-iphoneos/StickMe.build/Objects-normal/armv7/RadarView.d
@@ -1,4 +1,4 @@
 dependencies: \
   /Users/admin/Documents/Projects/StickMe/StickMe/StickMe/Views/RadarView.m \
   /Users/admin/Documents/Projects/StickMe/StickMe/StickMe/Views/RadarView.h \
-  /Users/admin/Documents/Projects/StickMe/StickMe/StickMe/../../../Stick'n'Find/Stick'n'Find/Stick'n'Find/Utilities/Utilities.h
+  /Users/admin/Documents/Projects/StickMe/StickMe/StickMe/Utilities/Utilities.h
